4573bb68a29a9896e3e9488f4483a97d4798909b
Anekenonso <okenwanonsokenneth@gmail.com>
2019-07-22 17:27:18 +0100

    added a few things to spice it up date: 22/07/2019

---
+++ src/components/Panel.js
@@ -0,0 +1,25 @@
+/* might use this latter
+
+import React from 'react';
+
+function Panel({ children, style = {} }) {
+  return (
+    <p
+      style={{
+        fontSize: '0.9em',
+        border: '1px solid var(--hr)',
+        borderRadius: '0.75em',
+        padding: '0.75em',
+        background: 'var(--inlineCode-bg)',
+        wordBreak: 'keep-all',
+        ...style,
+      }}
+    >
+      {children}
+    </p>
+  );
+}
+
+export default Panel;
+
+*/
